7589accf7cdcd7c09a76f06d58bdc0a6604383b3
Isaiah Shiner <mr.bob7138@gmail.com>
2015-04-27 10:22:20 -0400

    Added Tic
    Began work on the Tic system, not yet finished.

---
+++ src/runners/ClockTester.java
@@ -0,0 +1,12 @@
+package runners;
+
+import clock.Tic;
+
+public class ClockTester
+{
+	public static void main(String[] args)
+	{
+		Tic t = new Tic(000010);//means 10 seconds
+		System.out.println(t);
+	}
+}
--- src/runners/GeneralRunner.java
+++ src/runners/GeneralRunner.java
@@ -1,13 +1,15 @@
-package bot;
+package runners;
 
 import java.awt.AWTException;
+
+import bot.StockBot;
 
 public class GeneralRunner 
 {
 	public static void main(String[] args) throws AWTException 
 	{
 		StockBot bob = new StockBot();
-
+		System.out.println(bob);
 	}
 
 }
