92d64f7be55e7737287973f508658a76a81bab79
Isaiah Shiner <mr.bob7138@gmail.com>
2015-04-30 11:10:02 -0400

    Small changes
    Added some comments to the methods in the Clock class while adding the
    tooVolatile boolean variable

--- src/clock/Clock.java
+++ src/clock/Clock.java
@@ -14,6 +14,7 @@ public class Clock
 	private final String TRADE_INCREMENT;
 	private final String STOP_INCREMENT;
 	private final String FAST_INCREMENT = "000015";//15 seconds
+	private boolean tooVolatile = false;
 	/*
 	 * The clock thinks in military time strings.
 	 * These final references are when the market opens and closes,
@@ -22,13 +23,20 @@ public class Clock
 	public final String marketOpen =  "093000";//9:30:00 AM 
 	public final String marketClose = "163000";//4:30:00 PM
 	//								  "012345" index references
-	
+	/*
+	 * Default constructor: starts with 15 minute trade checking increments
+	 * and 1 minute stoploss checking increments
+	 */
 	public Clock()
 	{
 		TRADE_INCREMENT =  "001500";//15 minutes
 		STOP_INCREMENT =   "000100";//1 minute 
 	}
-	
+	/*
+	 * Overloaded constructor: let's the user input custom increments for 
+	 * trade and stoploss checking increments
+	 * USES "HHMMSS" for what the input should be
+	 */
 	public Clock(String t, String s)
 	{
 		TRADE_INCREMENT =  t;
@@ -42,7 +50,7 @@ public class Clock
 	public void run()
 	{
 		String timeStamp = null;
-		while(true)
+		while(!tooVolatile)
 		{
 			timeStamp = Clock.currentTime();
 			System.out.println(timeStamp);
@@ -50,11 +58,12 @@ public class Clock
 				
 		}
 	}
-	
-	public static String currentTime()
+	// Returns the current time in a String
+	public static String getCurrentTime()
 	{
 		return (new SimpleDateFormat("HHmmss").format(Calendar.getInstance().getTime()));
 	}
+	
 	private boolean isSeconds(String i)//i for increment
 	{
 		int currentSeconds = Integer.parseInt(Clock.currentTime().substring(4));
